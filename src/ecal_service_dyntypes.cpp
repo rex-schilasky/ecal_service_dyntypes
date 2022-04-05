e32d5553a9b45c2de8f282db45b81347ff2a8cf8
rex-schilasky <49162693+rex-schilasky@users.noreply.github.com>
2022-04-05 12:50:20 +0200

    split into smaller units

--- src/ecal_service_dyntypes.cpp
+++ src/ecal_service_dyntypes.cpp
@@ -4,72 +4,80 @@
 #include <iostream>
 #include <memory>
 
-class MathServiceImpl : public MathService
+bool GetServiceMessageDescFromType(const google::protobuf::ServiceDescriptor* service_desc_, const std::string& type_name_, std::string& type_desc_, std::string& error_s_ )
 {
-public:
-  virtual void Add     (::google::protobuf::RpcController* /* controller_ */, const ::SFloatTuple* /*request_*/, ::SFloat* /*response_*/, ::google::protobuf::Closure* /* done_ */) {}
-  virtual void Multiply(::google::protobuf::RpcController* /* controller_ */, const ::SFloatTuple* /*request_*/, ::SFloat* /*response_*/, ::google::protobuf::Closure* /* done_ */) {}
-};
-
-void GetMessageType(const google::protobuf::ServiceDescriptor* service_desc_, const std::string& type_name_, std::string& type_)
-{
-  std::string error_s;
+  std::shared_ptr<eCAL::protobuf::CProtoDynDecoder> msg_decoder = std::make_shared<eCAL::protobuf::CProtoDynDecoder>();
 
   const google::protobuf::FileDescriptor* file_desc = service_desc_->file();
+  if (!file_desc) return false;
 
   std::string file_desc_s = file_desc->DebugString();
   google::protobuf::FileDescriptorProto file_desc_proto;
-  std::shared_ptr<eCAL::protobuf::CProtoDynDecoder> msg_decoder = std::make_shared<eCAL::protobuf::CProtoDynDecoder>();
-  msg_decoder->GetFileDescriptorFromString(file_desc_s, &file_desc_proto, error_s);
+  if (!msg_decoder->GetFileDescriptorFromString(file_desc_s, &file_desc_proto, error_s_)) return false;
 
   google::protobuf::FileDescriptorSet pset;
   google::protobuf::FileDescriptorProto* pdesc = pset.add_file();
   pdesc->CopyFrom(file_desc_proto);
 
-  auto req_msg = msg_decoder->GetProtoMessageFromDescriptorSet(pset, type_name_, error_s);
+  auto req_msg = msg_decoder->GetProtoMessageFromDescriptorSet(pset, type_name_, error_s_);
+  if (!req_msg) return false;
+
   auto req_desc = req_msg->GetDescriptor();
-  type_ = req_desc->DebugString();
+  if (!req_desc) return false;
+
+  // TODO: Extract "real" protobuf descriptors here (not that human readable ones).
+  type_desc_ = req_desc->DebugString();
+
+  return true;
 }
 
-void GetServiceMessageTypes(const google::protobuf::ServiceDescriptor* service_desc_, std::map<std::string, std::string>& types_)
+bool GetServiceMessageTypeFromMethod(const google::protobuf::MethodDescriptor* method_desc_, std::string& req_type_, std::string& resp_type_)
 {
-  auto mcount = service_desc_->method_count();
-  for (auto m = 0; m < mcount; ++m)
-  {
-    std::string msg_type;
-    std::string msg_type_name;
-    auto method_desc = service_desc_->method(m);
+  if (!method_desc_) return false;
+  req_type_  = method_desc_->input_type()->name();
+  resp_type_ = method_desc_->output_type()->name();
+  return true;
+}
 
-    msg_type_name = method_desc->input_type()->name();
-    GetMessageType(service_desc_, msg_type_name, msg_type);
-    types_[msg_type_name] = msg_type;
+bool GetServiceMessageDescFromMethod(const google::protobuf::ServiceDescriptor* service_desc_, const google::protobuf::MethodDescriptor* method_desc_, std::string& req_desc_, std::string& resp_desc_, std::string& error_s_)
+{
+  std::string req_type, resp_type;
 
-    msg_type_name = method_desc->output_type()->name();
-    GetMessageType(service_desc_, msg_type_name, msg_type);
-    types_[msg_type_name] = msg_type;
-  }
+  if (!GetServiceMessageTypeFromMethod(method_desc_, req_type, resp_type))  return false;
+  if (!GetServiceMessageDescFromType(service_desc_, req_type, req_desc_, error_s_))   return false;
+  if (!GetServiceMessageDescFromType(service_desc_, resp_type, resp_desc_, error_s_)) return false;
+  
+  return true;
 }
 
 int main(int /*argc*/, char** /*argv*/)
 {
+  class MathServiceImpl : public MathService {};
   std::shared_ptr<MathService> math_service = std::make_shared<MathServiceImpl>();
 
   const google::protobuf::ServiceDescriptor* service_desc = math_service->GetDescriptor();
-  std::map<std::string, std::string> types_map;
-  GetServiceMessageTypes(service_desc, types_map);
-
   auto mcount = service_desc->method_count();
   for (auto m = 0; m < mcount; ++m)
   {
     auto method_desc = service_desc->method(m);
-    auto req_type = method_desc->input_type()->name();
-    auto resp_type = method_desc->output_type()->name();
-    std::cout << "mname          : " << method_desc->name() << std::endl;
-    std::cout << "req_type       : " << req_type << std::endl;
-    std::cout << "req_type_desc  : " << types_map[req_type] << std::endl;
-    std::cout << "resp_type      : " << resp_type << std::endl;
-    std::cout << "resp_type_desc : " << types_map[resp_type] << std::endl;
+
+    std::string req_type, resp_type;
+    GetServiceMessageTypeFromMethod(method_desc, req_type, resp_type);
+
+    std::string req_desc, resp_desc, error_s;
+    if (!GetServiceMessageDescFromMethod(service_desc, method_desc, req_desc, resp_desc, error_s))
+    {
+      std::cout << "Could not get service message descriptors, error: " << error_s << std::endl;
+    }
+    else
+    {
+      std::cout << "mname          : " << method_desc->name() << std::endl;
+      std::cout << "req_type       : " << req_type << std::endl;
+      std::cout << "req_type_desc  : " << req_desc << std::endl;
+      std::cout << "resp_type      : " << resp_type << std::endl;
+      std::cout << "resp_type_desc : " << resp_desc << std::endl;
+    }
   }
 
-  return(0);
+  return 0;
 }
